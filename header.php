0a974ac45a9c1557c775ce47b66c3da3156bed86
nesidos <141236631+nesiidos@users.noreply.github.com>
2025-05-06 12:17:28 +0900

    jai ajoute bouton KR et menu burger

---
+++ header.php
@@ -0,0 +1,8 @@
+<div class="header-controls">
+  <button id="languageToggle">KR</button>
+  <div class="menu-burger" id="menuBurger">
+    <span></span>
+    <span></span>
+    <span></span>
+  </div>
+</div>
