4c93529b451d8363155dc2fcf6fcf5db80544f5d
Xuan-Minh <naux.pro@gmail.com>
2025-05-06 04:32:25 +0200

    Link rel icon et pas apple touch icon

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <title>Glaneurs de carton</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="description" content="Glaneurs de carton" />
-    <link rel="apple-touch-icon" href="img/favicon.png" type="image/png" />
+    <link rel="icon" href="img/favicon.png" type="image/png" />
     <link rel="stylesheet" type="text/css" href="css/main.css" />
   </head>
   <body>
