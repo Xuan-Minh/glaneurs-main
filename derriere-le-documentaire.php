0c47960ee09b666300983977036a0000584ef894
Xuan-Minh <naux.pro@gmail.com>
2025-05-28 09:37:47 +0200

    Portfolio

--- derriere-le-documentaire.php
+++ derriere-le-documentaire.php
@@ -22,83 +22,99 @@ for ($i = 1; $i <= 16; $i++) {
      [
         "nom" => "Sakina DOUIOU",
         "roles" => ["Réalisatrice", "Directrice de projet","Scénariste", "Photographe","Chef opératrice", "Cadreuse", "Monteuse", "Etalonnage", "Repérage"],
-        "image" => "img/equipe/membre2.jpg"
+        "image" => "img/equipe/membre2.jpg",
+        "portfolio" => "https://sakinadouiou.github.io"
     ],
     [
         "nom" => "Xuan-Minh TRAN",
         "roles" => ["Directeur de projet", "Développeur web", "Prise de contact", "Technicien sonore", "Interprète", "Perch-man", "Repérage"],
-        "image" => "img/equipe/membre1.jpg"
+        "image" => "img/equipe/membre1.jpg",
+         "portfolio" => "https://xuan-minh.github.io/"
     ],
    
       [
         "nom" => "Dylan BLANDEL",
         "roles" => ["Responsable montage FR", "Scénariste", "Graphiste", "Cadreur"],
-        "image" => "img/equipe/membre1.jpg"
+        "image" => "img/equipe/membre1.jpg",
+        "portfolio" => ""
     ],
     [
         "nom" => "Hyunbeom PARK",
         "roles" => ["Compositeur musique originale", "Monteur son", "Interprète coréen", "Repérage"],
-        "image" => "img/equipe/membre2.jpg"
+        "image" => "img/equipe/membre2.jpg",
+        "portfolio" => ""
     ], 
      [
         "nom" => "Hugo LEULIET",
         "roles" => ["Perch-man", "Monteur son", "Compositeur musique originale"],
-        "image" => "img/equipe/membre1.jpg"
+        "image" => "img/equipe/membre1.jpg",
+        "portfolio" => ""
     ],
     [
         "nom" => "Romane VARO-TUPIN",
         "roles" => ["Assistante de production", "Prise de contact", "Organisatrice", "Transcription" ,"Sous-titreuse"],
-        "image" => "img/equipe/membre2.jpg"
+        "image" => "img/equipe/membre2.jpg",
+        "portfolio" => ""
     ],
     [
         "nom" => "Jiwoo YUN",
         "roles" => ["Transcripteuse", "Repérage"],
-        "image" => "img/equipe/membre1.jpg"
+        "image" => "img/equipe/membre1.jpg",
+        "portfolio" => ""
     ],
      [
         "nom" => "Suyeon YANG",
         "roles" => ["Interprète", "Repérage"],
-        "image" => "img/equipe/membre2.jpg"
+        "image" => "img/equipe/membre2.jpg",
+        "portfolio" => ""
     ],
        [
         "nom" => "Alyssia BERSET",
         "roles" => ["Responsable Montage KR", "Repérage", "Interprète"],
-        "image" => "img/equipe/membre1.jpg"
+        "image" => "img/equipe/membre1.jpg",
+        "portfolio" => ""
     ],
       [
         "nom" => "Inès DOS SANTOS",
         "roles" => ["Webmaster", "UX/UI designer", "Maquettiste", "Graphiste"],
-        "image" => "img/equipe/membre1.jpg"
+        "image" => "img/equipe/membre1.jpg",
+        "portfolio" => ""
     ],
     [
         "nom" => "Sarah CROS",
         "roles" => ["Graphiste", "UX/UI designer","Maquettiste" ,"Monteuse"],
-        "image" => "img/equipe/membre2.jpg"
+        "image" => "img/equipe/membre2.jpg",
+        "portfolio" => ""
     ],
     [
         "nom" => "Jaeden DUONG",
         "roles" => ["Traductrice","Transcripteuse", "Repérage"],
-        "image" => "img/equipe/membre2.jpg"
+        "image" => "img/equipe/membre2.jpg",
+        "portfolio" => ""
     ],
       [
         "nom" => "Junghyun LIM",
         "roles" => ["Perch-man", "Cadreur" ,"Repérage"],
-        "image" => "img/equipe/membre1.jpg"
+        "image" => "img/equipe/membre1.jpg",
+        "portfolio" => ""
     ],
       [
         "nom" => "Jiyeong SIN",
         "roles" => ["Interprète", "Transcripteuse", "Repérage"],
-        "image" => "img/equipe/membre1.jpg"
+        "image" => "img/equipe/membre1.jpg",
+        "portfolio" => ""
     ],     
       [
         "nom" => "Stephane LEVY",
         "roles" => ["Corps Enseignant","Tutrice"],
-        "image" => "img/equipe/membre1.jpg"
+        "image" => "img/equipe/membre1.jpg",
+        "portfolio" => "https://stephanelevy.net/?lang=fr#"
     ],
     [
         "nom" => "Gihoon YU",
         "roles" => ["Corps Enseignant","Traducteur"],
-        "image" => "img/equipe/membre2.jpg"
+        "image" => "img/equipe/membre2.jpg",
+        "portfolio" => ""
     ],
 
 ];
@@ -116,6 +132,9 @@ for ($i = 1; $i <= 16; $i++) {
                         <div><?php echo htmlspecialchars($role); ?></div>
                     <?php endforeach; ?>
                 </div>
+                   <?php if (!empty($membre['portfolio'])): ?>
+                       <a href="<?php echo htmlspecialchars($membre['portfolio']); ?>" class="team-portfolio" target="_blank">Portfolio</a>
+                    <?php endif; ?>
             </div>
         </div>
     <?php endforeach; ?>
