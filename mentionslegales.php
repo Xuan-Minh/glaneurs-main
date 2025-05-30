a8a3e0e566a103651f8f407944438c8cf73275db
Xuan-Minh <naux.pro@gmail.com>
2025-05-31 00:50:52 +0200

    Mentions légales XD

--- mentionslegales.php
+++ mentionslegales.php
@@ -8,11 +8,30 @@
     <title><?php echo getTranslation("mentionslegales_titre", $lang)?></title>
 </head>
 <body>
-    <div class="team-intro">
+    <div class="team-intro mentions-legales">
     <h2><?php echo getTranslation("mentionslegales_titre", $lang)?></h2>
-    <p>
-        Notre documentaire est le fruit d’un travail collectif, guidé par une vision commune : donner la parole à ceux qu’on n’entend jamais, et révéler la dignité derrière chaque histoire. Découvrez l’équipe qui porte cette direction artistique et humaine.
-    </p>
+      <h3>Éditeur du site :<br></h3>
+<div> Glaneurs de carton<br>
+    Bâtiment C Bois de l’étang, Rue Galilée 77420, Champs-sur-Marne<br>
+    glaneursdecarton.docu@gmail.com</div>
+
+  <h3>Directeur de la publication :<br></h3>
+    <div> Xuan-Minh TRAN</div>
+  
+  <h3>Hébergeur :<br></h3>
+   <div> ????<br>
+    xxxxxx<br>
+    Téléphone : ???? 
+    </div>
+  
+  <h3>Propriété intellectuelle :<br></h3>
+    <div>
+        Tous les contenus présents sur ce site sont protégés par le droit d’auteur. Toute reproduction, représentation, modification, publication, adaptation de tout ou partie des éléments du site, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable.
+    </div>
+  <h3>Données personnelles :<br></h3>
+    <div>Ce site ne collecte pas de données personnelles à l’insu des utilisateurs. Pour toute question, contactez-nous à l’adresse ci-dessus.
+    </div>
+</div>
 </div>
     <?php include "includes/header.php"; ?>
     <?php include "includes/jsinclude.php"; ?>
