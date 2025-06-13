a8faa4ff76d565e431eea540b3856e71baca9277
Xuan-Minh <naux.pro@gmail.com>
2025-06-14 00:11:15 +0200

    Mentions légales Responsive

--- mentionslegales.php
+++ mentionslegales.php
@@ -15,36 +15,36 @@
   <div class="page-intro mentions-legales content-anim">
     <h2><?php echo getTranslation("mentionslegales_titre", $lang) ?></h2>
     <h3><?php echo getTranslation("mentionslegales_editeur", $lang) ?><br></h3>
-    <div>Master Cultures et Métiers du Web<br>
+    <p>Master Cultures et Métiers du Web<br>
       Bureau C110, Bâtiment Bois de l’Étang (bâtiment C)<br>
       77420 CHAMPS-SUR-MARNE<br>
-      glaneursdecarton.docu@gmail.com</div>
+      glaneursdecarton.docu@gmail.com</p>
 
     <h3><?php echo getTranslation("mentionslegales_directeur_titre", $lang) ?><br></h3>
-    <div> Thierry BONZON</div>
-    <div> contact@mastercmw.com</div>
-    <div> 01 60 95 71 79</div>
+    <p> Thierry BONZON</p>
+    <p> contact@mastercmw.com</p>
+    <p> 01 60 95 71 79</p>
 
     <h3><?php echo getTranslation("mentionslegales_conception", $lang) ?><br></h3>
-    <div> Sakina DOUIOU, Ines DOS SANTOS, Sarah CROS, Xuan-Minh TRAN</div>
+    <p> Sakina DOUIOU, Ines DOS SANTOS, Sarah CROS, Xuan-Minh TRAN</p>
 
     <h3><?php echo getTranslation("mentionslegales_dvt", $lang) ?><br></h3>
-    <div> Xuan-Minh TRAN</div>
+    <p> Xuan-Minh TRAN</p>
 
     <h3><?php echo getTranslation("mentionslegales_hebergeur_titre", $lang) ?><br></h3>
-    <div> <?php echo getTranslation("mentionslegales_hebergeur_nom", $lang) ?><br>
+    <p> <?php echo getTranslation("mentionslegales_hebergeur_nom", $lang) ?><br>
       <?php echo getTranslation("mentionslegales_hebergeur_adresse_ip", $lang) ?><br>
       34 avenue de l’Europe, Immeuble trident A, 38100 Grenoble<br>
       <?php echo getTranslation("mentionslegales_hebergeur_telephone", $lang) ?>
-    </div>
+    </p>
 
     <h3><?php echo getTranslation("mentionslegales_propriete_intellectuelle_titre", $lang) ?><br></h3>
-    <div>
+    <p>
       <?php echo getTranslation("mentionslegales_propriete_intellectuelle_texte", $lang) ?>
-    </div>
+    </p>
     <h3><?php echo getTranslation("mentionslegales_donnees_personnelles_titre", $lang) ?><br></h3>
-    <div><?php echo getTranslation("mentionslegales_donnees_personnelles_texte", $lang) ?>
-    </div>
+    <p><?php echo getTranslation("mentionslegales_donnees_personnelles_texte", $lang) ?>
+    </p>
   </div>
   </div>
   
