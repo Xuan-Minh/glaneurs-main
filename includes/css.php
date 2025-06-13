ebe1817a500ab6963523f35c44809991d3772d78
Xuan-Minh <naux.pro@gmail.com>
2025-06-13 14:23:48 +0200

    Opti

--- includes/css.php
+++ includes/css.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="css/main.css">
 <link rel="stylesheet" href="css/font.css" type="text/css" />
 <link rel="stylesheet" href="css/navbar.css" type="text/css" />
-<link rel="stylesheet" href="css/visionner.css" type="text/css" />
+<!-- <link rel="stylesheet" href="css/visionner.css" type="text/css" /> -->
 <link rel="icon" href="img/favicon.png" type="image/png" />
