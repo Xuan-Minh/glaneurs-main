fd37429d52e8e4dc0bd8605680119c463bde0261
Xuan-Minh <naux.pro@gmail.com>
2025-06-20 22:22:36 +0200

    web

--- includes/css.php
+++ includes/css.php
@@ -1,3 +1,5 @@
+
+<meta name="description" content="<?php echo getTranslation('meta_description', $lang); ?>">
 <link rel="stylesheet" href="css/main.css">
 <link rel="stylesheet" href="css/font.css" type="text/css" />
 <link rel="stylesheet" href="css/navbar.css" type="text/css" />
