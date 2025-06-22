0d0a93a47742a1d3c2f3d7db0370cf280441310a
Xuan-Minh <naux.pro@gmail.com>
2025-06-22 22:34:54 +0200

    autoscroll menu

--- js/main.js
+++ js/main.js
@@ -324,7 +324,7 @@ function scrollToAndTrigger(slideNumber) {
 
   // Simule un clic sur le H2 pour ouvrir le visionneur
   setTimeout(function () {
-   $slide.find("div.visionner-trigger-h3").trigger("click");
+   $slide.find("button.visionner-trigger-h3").trigger("click");
   }, 600);
 }
 // ----------------------------------------------- HOVER VIDEO ---------------------------------- //
