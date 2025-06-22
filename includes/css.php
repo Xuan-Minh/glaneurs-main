095d6c44e8a217000a485504d7fd4a84851578db
Xuan-Minh <naux.pro@gmail.com>
2025-06-22 18:55:49 +0200

    preload font

--- includes/css.php
+++ includes/css.php
@@ -5,4 +5,6 @@
 <link rel="stylesheet" href="css/font.css" type="text/css" />
 <link rel="stylesheet" href="css/navbar.css" type="text/css" />
 <link rel="stylesheet" href="css/visionner.css" type="text/css" />
+<link rel="preload" href="/font/Libre_Baskerville/LibreBaskerville-Regular.ttf" as="font" type="font/ttf" crossorigin>
+<link rel="preload" href="/font/Figtree/Figtree-VariableFont_wght.ttf" as="font" type="font/ttf" crossorigin>w
 <link rel="icon" href="img/favicon.png" type="image/png" />
