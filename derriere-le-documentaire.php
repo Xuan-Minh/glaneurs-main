44cd30a12442ff55877204b4f941a03a049f1892
Xuan-Minh <naux.pro@gmail.com>
2025-06-23 02:53:37 +0200

    id

--- derriere-le-documentaire.php
+++ derriere-le-documentaire.php
@@ -100,9 +100,9 @@
     </section>
     <section class="dld-remerciements content-anim">
          <h2><?php echo getTranslation('derriereledocumentaire_soutienstitre' , $lang); ?></h2>
-         <p class="preserve-lines last-paragraph">
+         <p class="preserve-lines">
             <?php echo getTranslation('derriereledocumentaire_soutiens', $lang); ?>
-         </p><p class="supporters-list last-paragraph">
+         </p><p class="supporters-list" id="last-paragraph">
             <?php
                 // On récupère tous les soutiens depuis la BDD, triés par ordre alphabétique
                 $supporters = $pdo->query("SELECT name FROM supporters ORDER BY name ASC")->fetchAll(PDO::FETCH_COLUMN);
