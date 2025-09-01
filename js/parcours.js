e2117cdf2e860df723a2ec528916fc46735467a1
Xuan-Minh <naux.pro@gmail.com>
2025-09-01 11:51:26 +0200

    Coordonées GPS

---
+++ js/parcours.js
@@ -0,0 +1,44 @@
+const parcours = [
+  {
+    name: "Départ",
+    url: "https://maps.app.goo.gl/u1R9US5hK9gfwUajN9",
+    lat: 35.12915419555454, // à compléter
+    lng: 129.08841790649618, // à compléter
+  },
+  {
+    name: "Recyclerie",
+    url: "https://maps.app.goo.gl/Xpn3n88xip2bfSvk7",
+    lat: 35.13021,
+    lng: 129.090342,
+  },
+  {
+    name: "Pharmacie",
+    url: "https://maps.app.goo.gl/hBh84EE6gHxwsg668",
+    lat: 35.133147,
+    lng: 129.088532,
+  },
+  {
+    name: "GS25 (Recharge SD)",
+    url: "https://maps.app.goo.gl/PsGnB8nigFWSU8CF7",
+    lat: 35.13473,
+    lng: 129.094523,
+  },
+  {
+    name: "Entrepôt",
+    url: "https://maps.app.goo.gl/wXbBf4N5w6zY4WdK6",
+    lat: 35.13419,
+    lng: 129.092408,
+  },
+  {
+    name: "Immeuble",
+    url: "https://maps.app.goo.gl/ap1dfavthjVW6tgF9",
+    lat: 35.133072,
+    lng: 129.091712,
+  },
+  {
+    name: "Arrivée (Recyclerie)",
+    url: "https://maps.app.goo.gl/Xpn3n88xip2bfSvk7",
+    lat: 35.13021,
+    lng: 129.090342,
+  },
+];
