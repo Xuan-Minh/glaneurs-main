6b057beb623313307b058169f53136f85578495f
Xuan-Minh <naux.pro@gmail.com>
2026-04-16 16:42:38 +0900

    feat: Mettre à jour les balises meta pour le référencement et la date de dernière modification dans le sitemap

--- index.php
+++ index.php
@@ -81,7 +81,7 @@ if (!$isMobile) {
     <title><?php echo getTranslation("index_titre", 'fr') ?></title>
     <meta name="description" content="<?php echo getTranslation('meta_description', $lang); ?>">
     <meta name="author" content="Sakina DOUIOU & Xuan-Minh TRAN">
-    <meta name="keywords" content="documentary, South Korea, poesia, historical, social issues">
+    <meta name="keywords" content="documentary, South Korea, poesia, historical, social issues, Sakina DOUIOU, Xuan-Minh TRAN, Les glaneurs de carton">
 
     <meta property="og:title" content="Les glaneurs de carton" />
     <meta property="og:type" content="website" />
